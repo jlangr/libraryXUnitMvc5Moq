6dbafe5cd69130741e4cb1d6aa22fbe0d04d9712
Jeff L <jeff@langrsoft.com>
2020-10-19 22:42:26 -0600

    second pass of getting .NET Core 3.1

---
+++ LibraryCore/Extensions/SystemWebMvcController/SystemWebMvcController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Extensions.SystemWebMvcController
+{
+    public static class Extensions
+    {
+        public static string SoleErrorMessage(this Controller controller, string modelKey)
+        {
+            Console.Write("Keys:");
+            foreach (var key in controller.ModelState.Keys)
+                Console.Write(" " + key);
+            var errors = controller.ModelState[modelKey]?.Errors;
+            if (errors != null && errors.Any())
+                return errors.First().ErrorMessage;
+            return "*** No errors ***";
+        }
+    }
+}
